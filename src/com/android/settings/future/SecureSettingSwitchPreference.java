5e5c43363a16f0aa84bd36d2ab72b67ab53f5a0c
Sankar <sankarapspk@users.noreply.github.com>
2019-01-28 09:17:31 -0800

    future

--- src/com/android/settings/future/SecureSettingSwitchPreference.java
+++ src/com/android/settings/future/SecureSettingSwitchPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.reloaded;
+package com.android.settings.future;
 
 import android.content.Context;
 import android.support.v14.preference.SwitchPreference;
